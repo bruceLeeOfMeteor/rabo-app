05ad0855115fc05666a2b2f8daf265d0c9fe9ebc
Arman Murzabulatov <kz.arman@gmail.com>
2020-03-01 19:53:08 +0100

    catch service error

--- src/app/employees/services/employees.service.ts
+++ src/app/employees/services/employees.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '../../../environments/environment';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { Employee } from '../interfaces/employee';
 import { Observable } from 'rxjs';
 
@@ -13,7 +13,10 @@ export class EmployeesService {
   env = environment.moduleConfig.employees;
   constructor(private http: HttpClient) {}
   getList(): Observable<Employee[]> {
-    return this.http.get<EmployeesResponse>(`${this.env.baseUrl}/employees`).pipe(map(response => response.data || []));
+    return this.http.get<EmployeesResponse>(`${this.env.baseUrl}/employees`).pipe(
+      catchError(err => []), // implement error logger here
+      map(response => response?.data || [])
+    );
   }
   create() {
     return this.http.post(`${this.env.baseUrl}/create`, {});
