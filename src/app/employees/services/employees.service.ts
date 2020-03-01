7e0f5747f631dc05d58146dc0b63a7768864de07
Arman Murzabulatov <kz.arman@gmail.com>
2020-03-01 15:20:05 +0100

    declare interface & type response

--- src/app/employees/services/employees.service.ts
+++ src/app/employees/services/employees.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 
 import { environment } from '../../../environments/environment';
 import { map } from 'rxjs/operators';
+import { Employee } from '../interfaces/employee';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'any'
@@ -10,10 +12,15 @@ import { map } from 'rxjs/operators';
 export class EmployeesService {
   env = environment.moduleConfig.employees;
   constructor(private http: HttpClient) {}
-  getList() {
-    return this.http.get(`${this.env.baseUrl}/employees`).pipe(map(response => response['data'] || []));
+  getList(): Observable<Employee[]> {
+    return this.http.get<EmployeesResponse>(`${this.env.baseUrl}/employees`).pipe(map(response => response.data || []));
   }
   create() {
     return this.http.post(`${this.env.baseUrl}/create`, {});
   }
+}
+
+interface EmployeesResponse {
+  status: string;
+  data: Employee[];
 }
