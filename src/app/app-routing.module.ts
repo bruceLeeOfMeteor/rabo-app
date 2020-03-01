b18c5d3588069428c670d3918d4cb5117d9485d8
Arman Murzabulatov <kz.arman@gmail.com>
2020-03-01 13:27:43 +0100

    Generate employees module & component & set up routes (loaded lazily)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },
+  {
+    path: 'employees',
+    loadChildren: () =>
+      import('./employees/employees.module').then(
+        m => m.EmployeesModule,
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
