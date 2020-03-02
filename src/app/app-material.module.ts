bfb6e5e71cacf2763b60546e7b48f9cd1f39dfab
Arman Murzabulatov <kz.arman@gmail.com>
2020-03-02 11:31:05 +0100

    add sidenav toggle button

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  exports: [MatToolbarModule, MatSidenavModule, MatListModule]
+  exports: [MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule]
 })
 export class AppMaterialModule {}
