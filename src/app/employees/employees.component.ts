ae4498b46b0ee2caf5cd088d8f6f353362425653
Arman Murzabulatov <kz.arman@gmail.com>
2020-03-02 13:49:19 +0100

    implement employees selector

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeesService } from './services/employees.service';
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { pluck } from 'rxjs/operators';
+import { pluck, map, mergeMap, tap } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateEmployeeDialogComponent } from './create-employee-dialog/create-employee-dialog.component';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Employee } from './interfaces/employee';
 
 @Component({
   selector: 'app-employees',
@@ -12,7 +14,12 @@ import { CreateEmployeeDialogComponent } from './create-employee-dialog/create-e
 })
 export class EmployeesComponent implements OnInit {
   isSmallScreen: boolean;
-  employees$;
+  employees$: Observable<Employee[]>;
+
+  selectedEmployeeId: string; // needed for highlighting selected employee in a list
+  selectedEmployeeIdSubject: BehaviorSubject<string> = new BehaviorSubject('');
+  selectedEmployee$: Observable<Employee>;
+
   constructor(
     private employeesService: EmployeesService,
     private _breakpointObserver: BreakpointObserver,
@@ -26,10 +33,21 @@ export class EmployeesComponent implements OnInit {
       .subscribe((m: boolean) => (this.isSmallScreen = m));
 
     this.employees$ = this.employeesService.getList();
+
+    this.selectedEmployee$ = this.selectedEmployeeIdSubject.pipe(
+      tap(id => (this.selectedEmployeeId = id)),
+      mergeMap(_ => this.employeesService.getList()), // ideally endpoint should be called with id parameter (Eg: getItem(id))
+      tap(list => (this.selectedEmployeeId = !!this.selectedEmployeeId ? this.selectedEmployeeId : list[0].id)),
+      map(list => list.find(employee => employee.id === this.selectedEmployeeId))
+    );
   }
 
   get sidenavMode() {
     return this.isSmallScreen ? 'over' : 'side';
+  }
+
+  selectEmployee(id: string) {
+    this.selectedEmployeeIdSubject.next(id);
   }
 
   openDialog(): void {
